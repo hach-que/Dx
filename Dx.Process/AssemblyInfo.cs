12fb746a2344f74ea605498a5d0907b9db982418
James Rhodes <jrhodes@redpointsoftware.com.au>
2013-10-06 20:48:18 +1100

    Add AssemblyInfo.cs to runtime and processor

---
+++ Dx.Process/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+[assembly: AssemblyTitle("Dx.Process")]
+[assembly: AssemblyDescription("The post-processor for Dx.  You don't need to distribute this with your application.")]
+[assembly: AssemblyCompany("Redpoint Software")]
+[assembly: AssemblyProduct("Dx.Process")]
+[assembly: AssemblyCopyright("Copyright Redpoint Software 2013")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
---
+++ Dx.Runtime/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+[assembly: AssemblyTitle("Dx.Runtime")]
+[assembly: AssemblyDescription("The runtime library for Dx.  It supports distributed applications.")]
+[assembly: AssemblyCompany("Redpoint Software")]
+[assembly: AssemblyProduct("Dx.Runtime")]
+[assembly: AssemblyCopyright("Copyright Redpoint Software 2013")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
